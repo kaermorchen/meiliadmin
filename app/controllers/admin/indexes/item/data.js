5ad8e3668a62b7693e82ba9df2bb4369e89fea47
Stanislav Romanov <kaermorchen@gmail.com>
2022-03-24 13:56:55 +0300

    Add data request

--- app/controllers/admin/indexes/item/data.js
+++ app/controllers/admin/indexes/item/data.js
@@ -1,9 +1,24 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
+import { trackedFunction } from 'ember-resources';
 
 export default class AdminIndexesItemDataController extends Controller {
-  @tracked searchText;
+  @tracked searchText = 'hello';
+
+  request = trackedFunction(this, async () => {
+    if (typeof this.searchText !== 'string' || this.searchText.length < 1) {
+      return null;
+    }
+
+    const response = await this.model.search(this.searchText);
+
+    return response;
+  });
+
+  get data() {
+    return this.request.value;
+  }
 
   @action
   searchTextChanged(value) {
