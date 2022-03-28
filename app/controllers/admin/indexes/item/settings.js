90ce45c36ffe7c1c212862e2e7a189e1c9c59dc8
Stanislav Romanov <kaermorchen@gmail.com>
2022-03-28 17:51:49 +0300

    Add setting RankingRules

--- app/controllers/admin/indexes/item/settings.js
+++ app/controllers/admin/indexes/item/settings.js
@@ -1,13 +1,43 @@
 import Controller from '@ember/controller';
-import { action } from '@ember/object';
+import { action, get } from '@ember/object';
 import arrayEquals from 'meilisearch-admin/utils/array-equals';
 
 export default class AdminIndexesItemSettingsController extends Controller {
   emptyObj = {};
 
+  get rankingRulesFields() {
+    const arr = [];
+    // eslint-disable-next-line ember/no-get
+    const rankingRules = get(this, 'model.settings.rankingRules');
+
+    Object.keys(this.model.stats.fieldDistribution).forEach((key) => {
+      const asc = `${key}:asc`;
+      const desc = `${key}:desc`;
+
+      if (!rankingRules.includes(asc)) {
+        arr.push(asc);
+      }
+
+      if (!rankingRules.includes(desc)) {
+        arr.push(desc);
+      }
+    });
+
+    return arr;
+  }
+
+  @action
+  selectNewRankingRule(value) {
+    this.model.settings.rankingRules.addObject(value);
+  }
+
+  @action
+  removeRankingRule(value) {
+    this.model.settings.rankingRules.removeObject(value);
+  }
+
   @action
   saveStrToJSON(prop, emptyValue, str) {
-    console.log(str, prop, emptyValue);
     if (typeof str !== 'string' || str === '') {
       this.model.settings[prop] = emptyValue;
     } else {
