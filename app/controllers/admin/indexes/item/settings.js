1d42e8783c93ae902faad84a8e48328fc775c7f1
Stanislav Romanov <kaermorchen@gmail.com>
2022-04-29 14:29:42 +0300

    Add ember-sortable

--- app/controllers/admin/indexes/item/settings.js
+++ app/controllers/admin/indexes/item/settings.js
@@ -1,5 +1,5 @@
 import Controller from '@ember/controller';
-import { action, get } from '@ember/object';
+import { action, get, set } from '@ember/object';
 import arrayEquals from 'meilisearch-admin/utils/array-equals';
 import { ContentSaveOutline } from 'ember-mdi/icons';
 
@@ -59,5 +59,10 @@ export default class AdminIndexesItemSettingsController extends Controller {
     });
 
     return this.model.index.updateSettings(settings);
+  }
+
+  @action
+  reorderItems(itemModels) {
+    set(this.model.settings, 'rankingRules', itemModels);
   }
 }
