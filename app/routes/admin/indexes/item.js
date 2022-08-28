613a5cb45aa6356995ca4019bf3646213027f488
Stanislav Romanov <kaermorchen@gmail.com>
2022-08-28 18:11:41 +0300

    Styled login window

--- app/routes/admin/indexes/item.js
+++ app/routes/admin/indexes/item.js
@@ -2,6 +2,6 @@ import Route from '@ember/routing/route';
 
 export default class AdminIndexesItemRoute extends Route {
   model({ uid }) {
-    return this.modelFor('admin').indexes.find((item) => item.uid === uid);
+    return this.modelFor('admin').find((item) => item.uid === uid);
   }
 }
