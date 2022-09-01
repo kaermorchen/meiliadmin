5009209062b701f3c8f75ed56c61b7e617cc015d
Stanislav Romanov <kaermorchen@gmail.com>
2022-09-01 13:11:32 +0300

    Add dpocument tabs

--- app/routes/admin/indexes/item/documents/document/edit.js
+++ app/routes/admin/indexes/item/documents/document/edit.js
@@ -1,12 +1,3 @@
 import Route from '@ember/routing/route';
-// import { hash } from 'rsvp';
 
-export default class AdminIndexesItemDocumentsDocumentEditRoute extends Route {
-  // model({ document_id }) {
-  //   const index = this.modelFor('admin.indexes.item');
-  //   return hash({
-  //     index,
-  //     document: index.getDocument(document_id),
-  //   });
-  // }
-}
+export default class AdminIndexesItemDocumentsDocumentEditRoute extends Route {}
