bb1993493b59eace96cee5d5964b1fb33d26b773
Stanislav Romanov <kaermorchen@gmail.com>
2022-09-06 14:57:55 +0300

    Removed excess table's components

--- app/components/royal-table/row.js
+++
@@ -1,9 +0,0 @@
-// import Component from '@glimmer/component';
-
-// export default class RoyalTableRowComponent extends Component {}
-
-import templateOnly from '@ember/component/template-only';
-
-const RoyalTableRowComponent = templateOnly();
-
-export default RoyalTableRowComponent;
