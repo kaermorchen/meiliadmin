dcc0a192904e8b8624a24c2676642515f59558a3
Stanislav Romanov <kaermorchen@gmail.com>
2022-09-09 18:19:52 +0300

    Small fixes

--- app/components/sidenav-background.js
+++ app/components/sidenav-background.js
@@ -1,8 +1,6 @@
 import Component from '@glimmer/component';
-import { action } from '@ember/object';
 
 export default class SidenavBackgroundComponent extends Component {
-  @action
   closeSidebar() {
     document.body.classList.remove('sidebar-is-opened');
   }
