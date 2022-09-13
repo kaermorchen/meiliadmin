99ce95a9cc4da579c7dc74d693c4cbb511b38d27
Stanislav Romanov <kaermorchen@gmail.com>
2022-09-13 18:28:41 +0300

    Add positional observer

--- app/components/toast.js
+++ app/components/toast.js
@@ -3,12 +3,39 @@ import { inject as service } from '@ember/service';
 import { registerDestructor } from '@ember/destroyable';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
+import { htmlSafe } from '@ember/template';
+
+const margin = 16;
 
 export default class ToastComponent extends Component {
   @service toasts;
 
   @tracked width;
   @tracked height;
+
+  get top() {
+    const queue = this.toasts.queue;
+
+    for (let i = 0, height = 0; i < queue.length; i++) {
+      height += margin;
+
+      if (queue[i] === this) {
+        return `top: ${height}px`;
+      }
+
+      height += queue[i].height;
+    }
+
+    return `top: ${margin}px`;
+  }
+
+  get right() {
+    return `right: ${margin}px`;
+  }
+
+  get style() {
+    return htmlSafe([this.top, this.right].join('; '));
+  }
 
   @action
   triggerOnClose() {
@@ -23,10 +50,8 @@ export default class ToastComponent extends Component {
 
   registerResizeObserver(el) {
     const ro = new ResizeObserver((entries) => {
-      for (let entry of entries) {
-        this.width = entry.contentRect.width;
-        this.height = entry.contentRect.height;
-      }
+      this.width = entries[0].borderBoxSize[0].inlineSize;
+      this.height = entries[0].borderBoxSize[0].blockSize;
     });
 
     ro.observe(el);
--- app/services/toasts.js
+++ app/services/toasts.js
@@ -1,8 +1,7 @@
 import Service from '@ember/service';
 import { tracked } from '@glimmer/tracking';
-import SuccessToast from '../components/success-toast';
 import { hbs } from 'ember-template-imports';
-import { fn } from '@ember/helper';
+import SuccessToast from '../components/success-toast';
 
 export default class ToastsService extends Service {
   @tracked container = [];
