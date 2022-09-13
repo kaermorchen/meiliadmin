c40cf6b64eb606813d643efd8fbe00b89af5ab86
Stanislav Romanov <kaermorchen@gmail.com>
2022-09-13 17:34:42 +0300

    Add ResizeObserver for tracking widht and height

--- app/components/toast.js
+++ app/components/toast.js
@@ -2,9 +2,13 @@ import Component from '@glimmer/component';
 import { inject as service } from '@ember/service';
 import { registerDestructor } from '@ember/destroyable';
 import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
 export default class ToastComponent extends Component {
   @service toasts;
+
+  @tracked width;
+  @tracked height;
 
   @action
   triggerOnClose() {
@@ -12,6 +16,26 @@ export default class ToastComponent extends Component {
   }
 
   @action
+  didInsert(el) {
+    this.registerInQueue();
+    this.registerResizeObserver(el);
+  }
+
+  registerResizeObserver(el) {
+    const ro = new ResizeObserver((entries) => {
+      for (let entry of entries) {
+        this.width = entry.contentRect.width;
+        this.height = entry.contentRect.height;
+      }
+    });
+
+    ro.observe(el);
+
+    registerDestructor(this, () => {
+      ro.disconnect();
+    });
+  }
+
   registerInQueue() {
     this.toasts.add(this);
 
