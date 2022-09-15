61b8a2b63206e5b15c550b464ae5022e35cd1b5e
Stanislav Romanov <kaermorchen@gmail.com>
2022-09-15 13:38:24 +0300

    Add invoker to monaco in edit form

--- app/controllers/admin/indexes/item/documents/document/edit.js
+++ app/controllers/admin/indexes/item/documents/document/edit.js
@@ -2,11 +2,18 @@ import Controller from '@ember/controller';
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
+import ActionInvoker from 'meiliadmin/lib/action-invoker';
 
 export default class AdminIndexesItemDocumentsDocumentEditController extends Controller {
   @service router;
 
   @tracked error;
+
+  constructor() {
+    super(...arguments);
+
+    this.invoker = new ActionInvoker();
+  }
 
   @action
   save(value) {
