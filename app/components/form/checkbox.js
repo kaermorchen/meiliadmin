96966e290a6ed0a26f08a09041dc06bf5e456139
Stanislav Romanov <kaermorchen@gmail.com>
2022-09-18 12:31:47 +0300

    Change icons for shown fields in table

--- app/components/form/checkbox.js
+++ app/components/form/checkbox.js
@@ -1,7 +1,16 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
+import { CheckboxOutline, CheckboxBlankOutline } from 'ember-mdi';
 
 export default class FormCheckboxComponent extends Component {
+  get checkedIcon() {
+    return this.args.checkedIcon ?? CheckboxOutline;
+  }
+
+  get uncheckedIcon() {
+    return this.args.uncheckedIcon ?? CheckboxBlankOutline;
+  }
+
   @action
   onChange() {
     if (this.args.onChange) {
--- app/components/form/table-settings.js
+++ app/components/form/table-settings.js
@@ -1,7 +1,11 @@
 import Component from '@glimmer/component';
 import { guidFor } from '@ember/object/internals';
+import { EyeOutline, EyeOffOutline } from 'ember-mdi';
 
 export default class FormTableSettingsComponent extends Component {
+  checkedIcon = EyeOutline;
+  uncheckedIcon = EyeOffOutline;
+
   get guid() {
     return guidFor(this);
   }
