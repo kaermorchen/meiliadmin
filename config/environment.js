abbea3eac5f43a4fe14290440dd0b90a7cf0063b
Stanislav Romanov <kaermorchen@gmail.com>
2022-10-06 17:25:38 +0300

    Add rootURL for production build

--- config/environment.js
+++ config/environment.js
@@ -40,7 +40,7 @@ module.exports = function (environment) {
   }
 
   if (environment === 'production') {
-    // here you can enable a production-specific feature
+    ENV.rootURL = '/meiliadmin/';
   }
 
   return ENV;
