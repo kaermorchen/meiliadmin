dbf79113082f2a4b3562405adffcadc246a35259
Stanislav Romanov <kaermorchen@gmail.com>
2022-10-17 15:16:07 +0300

    Refactored theme toggler

--- app/components/theme-toggler.js
+++ app/components/theme-toggler.js
@@ -2,31 +2,29 @@ import { action } from '@ember/object';
 import Component from '@glimmer/component';
 import { inject as service } from '@ember/service';
 import { WhiteBalanceSunny, ThemeLightDark, WeatherNight } from 'ember-mdi';
-import { tracked } from '@glimmer/tracking';
 
 export default class ThemeTogglerComponent extends Component {
   @service session;
 
-  @tracked theme;
-
-  constructor() {
-    super(...arguments);
-
-    this.theme = document.documentElement.dataset.theme;
-  }
-
   get themes() {
     return [
       { id: 'light', name: 'Light', icon: WhiteBalanceSunny },
-      { id: 'system', name: 'System', icon: ThemeLightDark },
+      { id: undefined, name: 'System', icon: ThemeLightDark },
       { id: 'dark', name: 'Dark', icon: WeatherNight },
     ];
   }
 
   @action
   changeTheme(theme) {
-    localStorage.setItem('theme', theme);
-    this.theme = theme;
-    document.documentElement.dataset.theme = theme;
+    this.session.set('data.theme', theme);
+  }
+
+  @action
+  didChangeTheme(el, [theme]) {
+    if (theme) {
+      document.documentElement.dataset.theme = theme;
+    } else {
+      delete document.documentElement.dataset.theme;
+    }
   }
 }
